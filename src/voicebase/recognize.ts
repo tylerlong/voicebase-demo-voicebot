a6997630247895fac94d87fae5141885d51d3ab0
Kevin Zeng <zengfenfei@gmail.com>
2018-02-06 15:59:44 +0800

    custom vocabulary as a command line argument

--- src/voicebase/recognize.ts
+++ src/voicebase/recognize.ts
@@ -4,6 +4,11 @@ import * as fs from 'fs'
 import delay from 'delay.ts'
 
 let mediaFile = process.argv[2]
+let customVocabularyFile = process.argv[3]
+
+let customVocabularyTerms = fs.readFileSync(customVocabularyFile).toString().trim().split(/[^A-Za-z0-9_\-]+/).filter(w => !!w);
+console.log('Custom vocabulary terms:\n' + customVocabularyTerms.join(', ') + '\n')
+
 let mediaConfig = {
     "speechModel": {
         "language": "en-US"
@@ -13,11 +18,13 @@ let mediaConfig = {
             "enableNumberFormatting": false
         }
     },
-    /*"vocabularies": [
+    "vocabularies": [
         {
-            "vocabularyName" :  "rc-vocab"
+            terms: customVocabularyTerms.map(name => ({ term: name }))
+            // { term: 'Embbnux', soundsLike: ['em-nux'] },
+
         }
-    ]*/
+    ]/**/
 }
 
 let config = require('../data/config.json')
@@ -38,14 +45,14 @@ async function main() {
         body
     })
     let resJson = await res.json()
-    let {mediaId, status} = resJson
+    let { mediaId, status } = resJson
     console.log('Media upload status', status)
 
-    while(true) {
-        await delay(10*1000)
+    while (true) {
+        await delay(8 * 1000)
         console.log('Checking...')
-        res = await fetch(`https://apis.voicebase.com/v3/media/${mediaId}/transcript/text`, {headers})
-        if(res.status!=200) {
+        res = await fetch(`https://apis.voicebase.com/v3/media/${mediaId}/transcript/text`, { headers })
+        if (res.status != 200) {
             continue
         }
         let transcript = await res.text();
